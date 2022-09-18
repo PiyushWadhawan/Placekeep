9af53a0c8c2d60b720d43d5735106128689f3d4f
PiyushWadhawan <wa.piyush@gmail.com>
2022-09-18 20:28:28 +0530

    Added update, delete and authentication

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Users from './user/pages/Users';
 import NewPlace from './places/pages/NewPlace';
 import UserPlaces from './places/pages/UserPlaces';
 import Error from './shared/pages/Error';
+import UpdatePlace from './places/pages/UpdatePlace';
+import Auth from './user/pages/Auth';
 
 const App = () => {
   return (
@@ -14,8 +16,10 @@ const App = () => {
       <main>
         <Routes>
           <Route path="/" element={<Users/>}/>
-          <Route path='/:userId/places' element={<UserPlaces/>}></Route>
+          <Route path="/:userId/places" element={<UserPlaces/>}></Route>
           <Route path="/places/new" element={<NewPlace/>}/>
+          <Route path="/places/:placeId" element={<UpdatePlace/>}/>
+          <Route path="/auth" element={<Auth/>}/>
           <Route path="*" element={<Error/>}/>
         </Routes>
       </main>
